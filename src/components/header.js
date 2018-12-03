6ea0c98227eb38e3ec43abc67ae35dababfd9e2b
Truong An <ancs21.ps@gmail.com>
2018-12-04 01:58:42 +0700

    Add test layout

--- src/components/header.js
+++ src/components/header.js
@@ -3,32 +3,63 @@ import PropTypes from 'prop-types'
 import React from 'react'
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
+  <header
+    id="header"
+    class="u-header u-header--bg-transparent u-header--abs-top"
   >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+    <div class="u-header__section">
+      <div id="logoAndNav" class="container">
+        <nav class="navbar navbar-expand u-header__navbar">
+          <a
+            class="navbar-brand u-header__navbar-brand u-header__navbar-brand-center u-header__navbar-brand-text-white"
+            href="../home/index.html"
+            aria-label="Front"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              x="0px"
+              y="0px"
+              width="46px"
+              height="46px"
+              viewBox="0 0 46 46"
+              style={{ marginBottom: 0 }}
+            >
+              <path
+                fill="#E1E4EA"
+                opacity=".65"
+                d="M23,41.1L23,41.1c-9.9,0-18-8-18-18l0,0c0-9.9,8-18,18-18h11.3c3.7,0,6.6,3,6.6,6.6v11.4C41,33,32.9,41.1,23,41.1z"
+              />
+              <path
+                class="fill-white"
+                opacity=".5"
+                d="M28,36L28,36c-9.9,0-18-8-18-18l0,0c0-9.9,8-18,18-18h11.3C43,0.1,46,3.1,46,6.7v11.4C46,28,38,36,28,36z"
+              />
+              <path
+                class="fill-white"
+                opacity=".7"
+                d="M18,46.1L18,46.1c-10,0-18-8-18-18l0,0c0-9.9,8-18,18-18h11.3c3.7,0,6.6,3,6.6,6.6v11.4C35.9,38.1,27.9,46.1,18,46.1z"
+              />
+              <path
+                class="fill-primary"
+                d="M17.4,34.1V18.4h10.2v2.9h-6.4v3.4H26v2.9h-4.8v6.5H17.4z"
+              />
+            </svg>
+            <span class="u-header__navbar-brand-text">Front</span>
+          </a>
+
+          <div class="ml-auto">
+            <Link
+              class="btn btn-sm btn-primary transition-3d-hover"
+              href="/"
+              target="_blank"
+            >
+              {siteTitle}
+            </Link>
+          </div>
+        </nav>
+      </div>
     </div>
-  </div>
+  </header>
 )
 
 Header.propTypes = {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { StaticQuery, graphql } from 'gatsby'
-
+import Layout from '../components/layout'
 export default () => (
   <StaticQuery
     query={graphql`
@@ -16,7 +16,43 @@ export default () => (
       }
     `}
     render={data => (
-      <div>
+      <Layout>
+        <main id="content" role="main">
+          <div
+            class="d-lg-flex"
+            style={{
+              background:
+                'url(https://ucarecdn.com/2f0f998a-dfbe-41b6-91fd-097e1ac59ac7/bg4.png) no-repeat right',
+            }}
+          >
+            <div class="container d-lg-flex align-items-lg-center min-height-lg-100vh space-bottom-2 space-top-4 space-bottom-lg-3 space-lg-0">
+              <div class="w-lg-60 w-xl-50 mt-lg-9">
+                <div class="mb-6">
+                  <h1 class="h2 text-white font-weight-normal">
+                    Trusted by over 33 500 people
+                  </h1>
+                  <p class="text-white-70">
+                    Innovative and functional web template product – Front
+                    brought to you by Htmlstream, a bunch of geeks scattered all
+                    around the world.
+                  </p>
+                  <p class="text-white-70">
+                    We also offer affordable, professional and trendy customized
+                    design solutions, solely for your own project!
+                  </p>
+                </div>
+
+                <a
+                  class="btn btn-white btn-wide text-primary transition-3d-hover"
+                  href="/"
+                >
+                  Hire Us
+                </a>
+              </div>
+            </div>
+          </div>
+        </main>
+
         {data.moviesAll.movies.map(item => (
           <div key={item.id}>
             <h1>
@@ -24,7 +60,7 @@ export default () => (
             </h1>
           </div>
         ))}
-      </div>
+      </Layout>
     )}
   />
 )
